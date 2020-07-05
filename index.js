28c6a9c402537f8b5981a42751e840ebb6e45dba
David <davidmartincohen@gmail.com>
2020-07-05 23:31:42 +0200

    :sparkles: keep screen from going black

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { name as appName } from './app.json';
+import KeepAwake from 'react-native-keep-awake';
 
-const Main = () => (
-  <PaperProvider>
-    <App />
-  </PaperProvider>
-);
+const Main = () => {
+  useEffect(() => {
+    KeepAwake.activate();
+
+    return KeepAwake.deactivate();
+  }, []);
+
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+};
 
 AppRegistry.registerComponent(appName, () => Main);
